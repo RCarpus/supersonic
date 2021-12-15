9b2b2086e2fbc474686957a1baeba2de9ba13168
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-14 22:48:49 -0500

    Create mock-userData.js

---
+++ app/src/mock-userData.js
@@ -0,0 +1,10 @@
+export const mockUserData = {
+  Username: 'Gimpurr',
+  Email: 'ryancarpus@gmail.com',
+  Settings: {
+    NoteDuration: 'MEDIUM',
+    SoundWaveType: 'SINE',
+    PracticeStyle: 'MELODIC' // need to add this into API
+  },
+  Stats: {}
+};
