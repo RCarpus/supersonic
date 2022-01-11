166c759cc195a27cdb6f2b08f5ae70d9dcdc50e8
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-01-11 11:49:22 -0500

    settings read and save to localStorage, disconnected from redux store

--- app/src/components/SettingsPage/SettingsPage.jsx
+++ app/src/components/SettingsPage/SettingsPage.jsx
@@ -1,71 +1,77 @@
 // import libraries
 import React from 'react';
-import Axios from 'axios';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-
-// import actions
-import { setUserData } from '../../actions/actions';
-
-// import necessary components
-// import SomeComponent from '../components/SomeComponent';
-/* ---------------------- */
 
 // import stylesheet
 import './SettingsPage.scss';
 
-// import mockUserData
-import { mockUserData } from '../../mock-userData';
-setUserData(mockUserData);
-
-class SettingsPage extends React.Component {
+export default class SettingsPage extends React.Component {
   constructor(props) {
     super(props);
+    const defaultSettings = {
+      /**
+       * Default settings are used when no data is saved in localStorage
+       */
+      noteDuration: 'MEDIUM',
+      soundWaveType: 'SINE',
+      practiceStyle: 'MELODIC',
+    };
+
+    // Try to pull settings from localStorage, uses defaults if not found
+    let settings = JSON.parse(localStorage.getItem('settings')) || defaultSettings;
+
     this.state = {
-      noteDuration: mockUserData.Settings.NoteDuration,
-      soundWaveType: mockUserData.Settings.SoundWaveType,
-      practiceStyle: mockUserData.Settings.PracticeStyle
+      settings: {
+        noteDuration: settings.noteDuration,
+        soundWaveType: settings.soundWaveType,
+        practiceStyle: settings.practiceStyle,
+      }
     };
   }
 
   componentDidMount() {
-    document.getElementById(`${this.state.noteDuration}-button`).classList.add('active');
-    document.getElementById(`${this.state.soundWaveType}-button`).classList.add('active');
-    document.getElementById(`${this.state.practiceStyle}-button`).classList.add('active');
+    document.getElementById(`${this.state.settings.noteDuration}-button`).classList.add('active');
+    document.getElementById(`${this.state.settings.soundWaveType}-button`).classList.add('active');
+    document.getElementById(`${this.state.settings.practiceStyle}-button`).classList.add('active');
   }
 
   clickNoteDuration(value) {
-    this.setState({ noteDuration: value });
-    document.getElementById('SHORT-button').classList.remove('active');
-    document.getElementById('MEDIUM-button').classList.remove('active');
-    document.getElementById('LONG-button').classList.remove('active');
+    // updates the noteDuration setting in localStorage
+    // updates the UI accordinly
+    this.updateSettings('noteDuration', value);
+
+    document.querySelectorAll('.settings__note-duration__button')
+      .forEach(elem => {elem.classList.remove('active')});
     document.getElementById(`${value}-button`).classList.add('active');
   }
 
   clickSoundWaveType(value) {
-    this.setState({ soundWaveType: value });
-    document.getElementById('SINE-button').classList.remove('active');
-    document.getElementById('SQUARE-button').classList.remove('active');
-    document.getElementById('SAWTOOTH-button').classList.remove('active');
+    // updates the soundWaveType setting in localStorage
+    // updates the UI accordingly
+    this.updateSettings('soundWaveType', value);
+
+    document.querySelectorAll('.settings__sound-wave-button')
+      .forEach(elem => {elem.classList.remove('active')});
     document.getElementById(`${value}-button`).classList.add('active');
-    console.log(`clicked ${value}`); 
   }
 
   clickPracticeStyle(value) {
-    this.setState({ practiceStyle: value });
-    document.getElementById('MELODIC-button').classList.remove('active');
-    document.getElementById('HARMONIC-button').classList.remove('active');
+    // updates the practiceStyle setting in localStorage
+    // updates the UI accordingly
+    this.updateSettings('practiceStyle', value);
+
+    document.querySelectorAll('.settings__practice-style__button')
+      .forEach(elem => {elem.classList.remove('active')});
     document.getElementById(`${value}-button`).classList.add('active');
-    console.log(`clicked ${value}`); 
   }
 
-  clickSaveSettings() {
-    // Send a PUT request to the server
-    // Save results into new userData
-    // this.props.setUserData(response.data);
-    console.log('Saved settings (for pretend)');
+  updateSettings(setting, value) {
+    // updates the setting in localStorage
+    let updatedSettings = this.state.settings;
+    updatedSettings[setting] = value;
+    this.setState({ settings: updatedSettings });
+    localStorage.setItem('settings', JSON.stringify(this.state.settings));
   }
-
 
   render() {
     const { settings } = this.props;
@@ -74,54 +80,42 @@ class SettingsPage extends React.Component {
         <h2 className="settings__title">Settings</h2>
         <div className="settings__note-duration">
           <p className="settings__note-duration__label">Note duration:</p>
-          <button onClick={ () => { this.clickNoteDuration("SHORT")} } 
-                  className="settings__note-duration__button"
-                  id="SHORT-button">short</button>
-          <button onClick={ () => {  this.clickNoteDuration("MEDIUM")} } 
-                  className="settings__note-duration__button"
-                  id="MEDIUM-button">medium</button>
-          <button onClick={ () => { this.clickNoteDuration("LONG")}} 
-                  className="settings__note-duration__button"
-                  id="LONG-button">long</button>
+          <button onClick={() => { this.clickNoteDuration("SHORT") }}
+            className="settings__note-duration__button"
+            id="SHORT-button">short</button>
+          <button onClick={() => { this.clickNoteDuration("MEDIUM") }}
+            className="settings__note-duration__button"
+            id="MEDIUM-button">medium</button>
+          <button onClick={() => { this.clickNoteDuration("LONG") }}
+            className="settings__note-duration__button"
+            id="LONG-button">long</button>
         </div>
         <div className="settings__sound-wave-type">
           <p className="settings__sound-wave-type__label">Sound wave type</p>
-          <button onClick={ () => { this.clickSoundWaveType("SINE")} } 
-                  className="settings__sound-wave-button"
-                  id="SINE-button">sine</button>
-          <button onClick={ () => { this.clickSoundWaveType("SQUARE")} } 
-                  className="settings__sound-wave-button"
-                  id="SQUARE-button">square</button>
-          <button onClick={ () => { this.clickSoundWaveType("SAWTOOTH")} } 
-                  className="settings__sound-wave-button"
-                  id="SAWTOOTH-button">sawtooth</button>
+          <button onClick={() => { this.clickSoundWaveType("SINE") }}
+            className="settings__sound-wave-button"
+            id="SINE-button">sine</button>
+          <button onClick={() => { this.clickSoundWaveType("SQUARE") }}
+            className="settings__sound-wave-button"
+            id="SQUARE-button">square</button>
+          <button onClick={() => { this.clickSoundWaveType("SAWTOOTH") }}
+            className="settings__sound-wave-button"
+            id="SAWTOOTH-button">sawtooth</button>
         </div>
         <div className="settings__practice-style">
           <p className="settings__practice-style__label">Practice style:</p>
-          <button onClick={ () => { this.clickPracticeStyle("MELODIC")} }
-                  className="settings__practice-style__button"
-                  id="MELODIC-button">melodic</button>
-          <button onClick={ () => { this.clickPracticeStyle("HARMONIC")} }
-                  className="settings__practice-style__button"
-                  id="HARMONIC-button">harmonic</button>
+          <button onClick={() => { this.clickPracticeStyle("MELODIC") }}
+            className="settings__practice-style__button"
+            id="MELODIC-button">melodic</button>
+          <button onClick={() => { this.clickPracticeStyle("HARMONIC") }}
+            className="settings__practice-style__button"
+            id="HARMONIC-button">harmonic</button>
         </div>
-        <button onClick={ this.clickSaveSettings }
-                className="settings__save-settings-button">Save</button>
-        <button onClick={ () => {history.back()} }
-                className="settings__back-button">Back</button>
+        <button onClick={() => { history.back() }}
+          className="settings__back-button">Back</button>
 
       </div>
 
     )
   }
 }
-
-// Add anything needed in this component from the global state
-let mapStateToProps = state => {
-  return {
-    userData: state.userData,
-  }
-}
-
-// The second parameter object contains the state actions we imported at the top
-export default connect(mapStateToProps, { setUserData })(SettingsPage);
