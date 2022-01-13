071b7bd61e3c9ffaec86228635ba01868cdea8a8
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-01-13 14:34:25 -0500

    implemented profile page with fields to edit username, password, and email

--- app/src/components/ProfilePage/ProfilePage.jsx
+++ app/src/components/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,8 @@
 // import libraries
 import React from 'react';
 import Axios from 'axios';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-
-// import actions
-// no actions needed for this element
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 // import necessary components
 // import SomeComponent from '../components/SomeComponent';
@@ -13,13 +10,93 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 // import stylesheet
 
-class ProfileSetupPage extends React.Component {
+export default class ProfilePage extends React.Component {
+  constructor() {
+    super();
+    this.form = React.createRef();
+    this.updateUserData = this.updateUserData.bind(this);
+    // this.unregister = this.unregister.bind(this);
+  }
+
+  updateUserData() {
+    if (this.form.current.reportValidity()) {
+
+      const user = JSON.parse(localStorage.getItem('userData')).Username;
+      const token = JSON.parse(localStorage.getItem('token'));
+      console.log(user);
+      console.log(token);
+
+      let Username = this.form.current[0].value;
+      let Password = this.form.current[1].value;
+      let Email = this.form.current[2].value;
+      console.log(Username, Password, Email);
+      let updatedData = {};
+      if (Username.length > 0) updatedData.Username = Username;
+      if (Password.length > 0) updatedData.Password = Password;
+      if (Email.length > 0) updatedData.Email = Email;
+      console.log(updatedData);
+
+      let authHeader = { headers: { Authorization: `Bearer ${token}` } }
+      console.log(authHeader);
+      Axios.put(`https://supersonic-api.herokuapp.com/users/${user}`, updatedData, authHeader)
+        .then(response => {
+          window.alert('successfully updated user data');
+          console.log(response.data);
+          localStorage.setItem('userData', JSON.stringify(response.data.updatedUser));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    else {
+      console.log('invalid form');
+    }
+  }
 
   render() {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    const username = userData.Username;
+    const password = '*****';
+    const email = userData.Email;
     return (
       <div>
         <button className="logout-button" onClick={this.props.handleLogout}>logout</button>
-        <p>This is the Profile page.</p>
+        <div className="user-info">
+          <h2>Profile</h2>
+          <Form className="update-info-form" ref={this.form} onSubmit={e => e.preventDefault()}>
+
+            <Form.Group>
+              <Form.Label className="user-info__form-label">
+                Username
+              </Form.Label>
+              <Form.Control className="user-info__form-input" placeholder={username}
+                pattern="^[a-zA-Z0-9]{1,}$" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="user-info__form-label">
+                Password
+              </Form.Label>
+              <Form.Control className="user-info__form-input" placeholder={password} 
+                pattern=".{8,}"
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="user-info__form-label">
+                E-mail
+              </Form.Label>
+              <Form.Control className="user-info__form-input" placeholder={email}
+                pattern=".*@.*\..*" />
+            </Form.Group>
+
+
+            <Button className="btn btn-secondary btn-sm" variant="primary" type="submit" onClick={this.updateUserData} >Update</Button>
+
+          </Form>
+
+        </div>
+
 
       </div>
 
@@ -34,5 +111,3 @@ let mapStateToProps = state => {
   }
 }
 
-// The second parameter object contains the state actions we imported at the top
-export default connect(mapStateToProps, null)(ProfileSetupPage);
