86fc81291bb82a97e6908fbb4cb13fe0d1df8c3b
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-01-20 12:46:36 -0500

    Created LoadingIndicator element

---
+++ app/src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LoadingIndicator.scss';
+
+export default class LoadingIndicator extends React.Component {
+  
+  render() {
+    return (
+      <div className='loading-message'>
+        <p className='loading-message__text'>Loading</p>
+      </div>
+    )
+  }
+}
