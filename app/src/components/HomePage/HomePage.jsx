71d4ecc13ccbd50ff4b7f468873ad433f6ce857a
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-01-21 07:58:06 -0500

    Created footer text and plugged into HomePage

--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -13,6 +13,7 @@ import PracticePage from '../PracticePage/PracticePage';
 import SettingsPage from '../SettingsPage/SettingsPage';
 import ProfilePage from '../ProfilePage/ProfilePage';
 import StatsPage from '../StatsPage/StatsPage';
+import Footer from '../Footer/Footer';
 // import stylesheet
 import './HomePage.scss';
 
@@ -21,30 +22,31 @@ class HomePage extends React.Component {
   render() {
     return (
       <div id="home-page">
-          <div id="home-page" className="page-content">
-            <div id="home-page__content__info">
-              <h2 id="home-page__subtitle">fine-tuned ear training</h2>
-              <p>By Ryan Carpus</p>
-            </div>
-            <div id="home-page__button-grid">
-              <Link to='/practice'>
-                <button className="home-page__button">Practice</button>
-              </Link>
-              <Link to='/profile'>
-                <button className="home-page__button">Profile</button>
-              </Link>
-              <Link to='/stats'>
-                <button className="home-page__button">Stats</button>
-              </Link>
-              <Link to='/settings'>
-                <button className="home-page__button">Settings</button>
-              </Link>
+        <div id="home-page" className="page-content">
+          <div id="home-page__content__info">
+            <h2 id="home-page__subtitle">fine-tuned ear training</h2>
+            <p>By Ryan Carpus</p>
+          </div>
+          <div id="home-page__button-grid">
+            <Link to='/practice'>
+              <button className="home-page__button">Practice</button>
+            </Link>
+            <Link to='/profile'>
+              <button className="home-page__button">Profile</button>
+            </Link>
+            <Link to='/stats'>
+              <button className="home-page__button">Stats</button>
+            </Link>
+            <Link to='/settings'>
+              <button className="home-page__button">Settings</button>
+            </Link>
 
-            </div>
 
           </div>
 
+        </div>
 
+        <Footer />
       </div >
 
     )
