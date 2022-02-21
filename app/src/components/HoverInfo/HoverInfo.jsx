629c9e8d130feaf2ed632c49184683afd3437c25
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-02-21 11:36:21 -0500

    update HoverInfo styles

--- app/src/components/HoverInfo/HoverInfo.jsx
+++ app/src/components/HoverInfo/HoverInfo.jsx
@@ -5,7 +5,7 @@ export default class extends React.Component {
   render() {
     const { title, text } = this.props;
     return (
-      <div className="hover-info" title="some text">
+      <div className="hover-info">
         <span className="hover-info__title">{title}</span>
         <p className="hover-info__text">{text}</p>
       </div>
